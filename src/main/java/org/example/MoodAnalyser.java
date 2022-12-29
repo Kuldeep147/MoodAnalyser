1d90ad74b929f0edc9caf989754afb9341362e0e
Kuldeep147 <kuldeep.bargurjar@gmail.com>
2022-12-29 18:52:20 +0530

    Adding uc1

--- src/main/java/org/example/MoodAnalyser.java
+++ src/main/java/org/example/MoodAnalyser.java
@@ -4,5 +4,10 @@ public class MoodAnalyser {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-    
+    public static String analyseMood(String str){
+        if(str.equals("I am in Sad Mood")){
+            return "SAD";
+        } else return "HAPPY";
+    }
+
 }
