1920b5e0d3d2041ed69d8767ffdd9649c8e5b980
Kuldeep147 <kuldeep.bargurjar@gmail.com>
2022-12-29 19:04:38 +0530

    Adding refactored code tc

--- src/test/java/TestMoodAnalyser.java
+++ src/test/java/TestMoodAnalyser.java
@@ -3,11 +3,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestMoodAnalyser {
+    MoodAnalyser mood = new MoodAnalyser("I am in Sad Mood");
+    MoodAnalyser mTwo = new MoodAnalyser("I am in Happy Mood");
+
     @Test
     public void testAnalyseMood(){
         assertEquals("SAD", MoodAnalyser.analyseMood("I am in Sad Mood"));
         assertEquals("HAPPY", MoodAnalyser.analyseMood("I am in Any Mood"));
         assertEquals("HAPPY", MoodAnalyser.analyseMood("I am in Mood"));
+        assertEquals("SAD",MoodAnalyser.analyseMood(mood.getMsg()));
+        assertEquals("HAPPY",MoodAnalyser.analyseMood(mTwo.getMsg()));
     }
 
 }
